802c1d726f0b6f34ccf79b00ce96e7c199de77ad
Rashawn <arctitan78@gmail.com>
2021-12-23 15:54:45 -0500

    updated the ex_app.py file to get the basic structure and layout of the code and tested inserting rows into the database using memory and the database.py file; next time start at "selection 1 / 2" under the while loop and get the code to print the output of the data

--- ex_app.py
+++ ex_app.py
@@ -1,59 +1,98 @@
 # Exercise App
+import csv
 import database
 from datetime import date
 from datetime import datetime
 
-prompt = "Welcom to the exercise log.\n"
+prompt = "Welcome to the exercise log.\n"
 print(prompt)
+selection = input("What would you like to do? 1=single entry, 2=multiple entires, \
+3=csv file, or '4' to exit. ")
+while True:
+    if selection == '4':
+        print('Goodbye')
+        break
+    elif selection == '3':
+        print("placeholder for csv file")
+        break
 
-# get user input for the date
-# note to self: use CAST TO re-format dates in the realtional database
-def today_date(date):
-    date = input("Provide the date (e.g 01/25/2022) or hit \'enter\' for today's date: ")
-    while date != 'exit'.lower():
-        if date == 'exit'.lower():
-            break
-        elif date == "":
-            tday = datetime.today()
-            tday = tday.strftime('%m/%d/%Y')
-            year = tday[6:10]
-            month = tday[:2]
-            return tday, int(year), int(month)
-        elif len(date)==10 and '/' in date:
-            tday = date
-            year = tday[6:10]
-            month = tday[:2]
-            return tday, int(year), int(month)
-        else:
-            print("DATE FORMAT ERROR! Month and day must be two digits.")
 
-        date = input("Provide the date (e.g 01/25/2022) or hit \'enter\' for today's date: ")
+    ### START HERE NEXT ###
+    # this section sucessfully takes the input...
+    # however, it does not print the output when calling the functions
+    # I think I need to change the indentation or...
+    # remove / alter a break from one of the if statements
+    elif selection == '2' or selection == '1':
+        log_lst = []
+        print("-----------test for selection 1 or 2 ------------------")
+        while True:
+            add_entry = input("Would you like to make an entry? (Y/N): ").upper()
+            if add_entry == 'N':
+                print("No further entries. Exiting the program...")
+                print("---------break successful----------------")
+                break
 
-# function to  get user input for miles walked
-def miles_walked():
-    miles = input("Enter miles walked or hit 'exit': ")
-    while miles != 'exit'.lower():
-        if miles == 'exit'.lower():
-            break
-        elif miles.isdigit() and '.' not in miles:
-            print(f'You walked {miles} miles.')
-            return int(miles)
-        elif miles.isdigit() and '.' in miles:
-            print(f'You walked {miles} miles.')
-            return int(miles)
-        else:
-            print("ERROR! Enter a digit.")
+            elif add_entry == 'Y':
+                print("---------------add_entry 'yes' test successful------------")
 
-        miles = input("Enter miles walked or hit 'exit': ")
+                # get user input for the date
+                def today_date(date):
+                    date = input("Provide the date (e.g 01/25/2022) or hit \'enter\' for today's date: ")
+                    while date != 'exit'.lower():
+                        if date == 'exit'.lower():
+                            break
+                        elif date == "":
+                            tday = datetime.today()
+                            tday = tday.strftime('%m/%d/%Y')
+                            year = tday[6:10]
+                            month = tday[:2]
+                            dym = tday, int(year), int(month)
+                            return dym
+                        elif len(date)==10 and '/' in date:
+                            tday = date
+                            year = tday[6:10]
+                            month = tday[:2]
+                            dym = tday, int(year), int(month)
+                            return dym
+                        else:
+                            print("DATE FORMAT ERROR! Month and day must be two digits.")
 
-### START HERE NEXT ###
-# test the user input
-# call the functions
-# append the output of the functions into a list
-lst = []
-for item in today_date(date):
-    lst.append(item)
-lst.append(miles_walked())
-exercise_log = lst
-database.add_one(exercise_log)
-print(database.show_all())
+                        date = input("Provide the date (e.g 01/25/2022) or hit \'enter\' for today's date: ")
+                # function to  get user input for miles walked
+                def miles_walked():
+                    miles = input("Enter miles walked or hit 'exit': ")
+                    while miles != 'exit'.lower():
+                        if miles == 'exit'.lower():
+                            break
+                        elif miles.isdigit() and '.' not in miles:
+                            print(f'You walked {miles} miles.')
+                            return int(miles)
+                        elif miles.isdigit() and '.' in miles:
+                            print(f'You walked {miles} miles.')
+                            return int(miles)
+                        else:
+                            print("ERROR! Enter a digit.")
+
+                        miles = input("Enter miles walked or hit 'exit': ")
+
+                # call the functions
+                # append the output of the functions into a list
+                lst = []
+                for item in today_date(date):
+                    lst.append(item)
+                lst.append(miles_walked())
+                print("------------ func call successful -----------------")
+                log_lst.append(lst)
+                print("-------------test list input -----------------------")
+                print(log_lst)
+                continue
+        break
+
+    # OUTER WHILE LOOP: exit if user does not select an entry 1-4
+    else:
+        print("Invalid entry. Exiting the program....")
+        break
+    # call the database.py functions to add records to the db
+    # database.add_one(log_lst)
+    database.add_many(log_lst)
+    print(database.show_all())
